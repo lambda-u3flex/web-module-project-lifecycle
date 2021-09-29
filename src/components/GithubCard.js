351a9e5027286ff3dfd6366494fab7d477e9d608
Tim Marchant <fromtimwithcode@gmail.com>
2021-09-29 00:45:05 -0500

    more styling and added github calendar

--- src/components/GithubCard.js
+++ src/components/GithubCard.js
@@ -5,18 +5,19 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { flexbox } from '@mui/system';
 
 const GithubCard = props => {
     return(
+        <>
         <Card sx={{ 
             maxWidth: 600,
-            backgroundColor: '#161618',
+            backgroundColor: '#f5f5f5',
             display: 'flex',
-            flexDirection: 'column',
+            flexDirection: 'row',
             justifyContent: 'center',
             alignItems: 'center',
-            color: '#fff'
+            color: '#000',
+            marginTop: '2rem'
             }}>
             <CardMedia
                 component="img"
@@ -25,20 +26,28 @@ const GithubCard = props => {
                 width="300"
                 image={props.myData.avatar_url}
             />
+            <div className="test">
             <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
                     {props.myData.name}
                 </Typography>
                 <Typography variant="body2" color="#fff">
                 <a href="https://www.github.com/fromtimwithcode"
-                    style={{ textDecoration: 'none', color: '#BB86FC', fontSize: '.8rem' }}>@{props.myData.login}</a>
+                    style={{ textDecoration: 'none', color: 'green', fontSize: '.8rem' }}>@{props.myData.login}</a>
                 </Typography>
             </CardContent>
             <CardActions>
-                <Button size="small" style={{color:'#BB86FC'}}>{props.myData.followers} Followers</Button>
-                <Button size="small" style={{color:'#BB86FC'}}>Visit Website</Button>
+                <Button size="small" style={{color:'green'}}>{props.myData.followers} Followers</Button>
+                <Button size="small" style={{color:'green'}} onClick={() => {
+                    window.open("https://www.fromtimwithcode.com", "_blank")
+                }}>Visit Website</Button>
             </CardActions>
+            </div>
         </Card>
+        <div>
+        <img src="http://ghchart.rshah.org/fromtimwithcode" alt="fromtimwithcode's Github chart" style={{marginTop: '1.5rem'}} />
+        </div>
+        </>
     )
 }
 
